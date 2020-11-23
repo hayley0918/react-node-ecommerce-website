2bd87a6ee5338905483bc3250503c7193c3ef846
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-11-24 07:32:18 +1100

    create data file and convert the li part to js form to render the data

---
+++ frontend/src/data.js
@@ -0,0 +1,49 @@
+export default {
+    products: [
+        {
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            category: 'Shirts',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numOfReviews: 10
+        },
+        {
+            name: 'Fit Shirt',
+            image: '/images/d1.jpg',
+            category: 'Shirts',
+            price: 80,
+            brand: 'Nike',
+            rating: 4.0,
+            numOfReviews: 20
+        },
+        {
+            name: 'Pants',
+            image: '/images/d1.jpg',
+            category: 'PAnts',
+            price: 100,
+            brand: 'Nike',
+            rating: 3.5,
+            numOfReviews: 30
+        },
+        {
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            category: 'Shirts',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numOfReviews: 10
+        },
+        {
+            name: 'Slim Shirt',
+            image: '/images/d1.jpg',
+            category: 'Shirts',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numOfReviews: 10
+        }
+    ]
+}
