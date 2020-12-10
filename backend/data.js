6903e5548934ec21d04270d458c8f692545ca749
Min Kyung CHOI <haleychoi0918@gmail.com>
2020-12-10 16:03:04 +1100

    convert product details page managed by redux and implement add to cart

--- backend/data.js
+++ backend/data.js
@@ -8,7 +8,8 @@ export default {
         price: 60,
         brand: ' Nike',
         rating: 4.5,
-        numReviews: 10
+        numReviews: 10,
+        countInStock: 6
       },
       {
         _id: '2',
@@ -18,7 +19,8 @@ export default {
         price: 50,
         brand: ' Nike',
         rating: 4.2,
-        numReviews: 5
+        numReviews: 5,
+        countInStock: 4
       },
       {
         _id: '3',
@@ -28,7 +30,8 @@ export default {
         price: 70,
         brand: ' Nike',
         rating: 4.5,
-        numReviews: 8
+        numReviews: 8,
+        countInStock: 7
       }, {
         _id: '4',
         name: 'Best Pants',
@@ -37,7 +40,8 @@ export default {
         price: 70,
         brand: ' Nike',
         rating: 4.5,
-        numReviews: 8
+        numReviews: 8,
+        countInStock: 2
       },
       {
         _id: '5',
@@ -47,17 +51,19 @@ export default {
         price: 70,
         brand: ' Nike',
         rating: 4.5,
-        numReviews: 8
+        numReviews: 8,
+        countInStock: 3
       },
       {
-        _id: '4',
+        _id: '6',
         name: 'Best Pants',
         category: 'Pants',
         image: '/images/d1.jpg',
         price: 70,
         brand: ' Nike',
         rating: 4.5,
-        numReviews: 8
+        numReviews: 8,
+        countInStock: 0
       }
     ]
   }
